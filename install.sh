74f5a4e387384db33221b3a598ef4c97a6e5c644
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2020-07-29 09:58:54 +0200

    Delete install.sh

--- install.sh
+++
@@ -1,4 +0,0 @@
-sudo mkdir /usr/share/icons/docker
-sudo cp resources/startfirefox.sh /usr/local/bin
-sudo cp resources/firefox-docker.desktop /usr/share/applications
-sudo cp resources/firefox.png /usr/share/icons/docker
