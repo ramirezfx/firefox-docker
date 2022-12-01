5b6ec6d564722af3375b6e97e4722258525fb557
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2022-12-01 13:38:51 +0100

    Add files via upload

---
+++ startfirefox.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+HOME="$(echo ~)"
+
+set -e
+
+if [[ -n "$(docker ps -qaf 'name=ramirezfx/firefox:latest')" ]]; then
+        docker restart ramirezfx/firefox:latest
+else
+        USER_UID=$(id -u)
+        USER_GID=$(id -g)
+        xhost +local:docker
+
+        docker run --shm-size=2g --rm \
+                --security-opt seccomp=unconfined \
+                --env=USER_UID=$USER_UID \
+                --env=USER_GID=$USER_GID \
+                --env=DISPLAY=unix$DISPLAY \
+                -v ${HOME}/docker/firefox-home:/home/firefox \
+                --volume=/tmp/.X11-unix:/tmp/.X11-unix:ro \
+                --volume=/run/user/$USER_UID/pulse:/run/pulse:ro \
+                --name firefox \
+                ramirezfx/firefox:latest
+fi
