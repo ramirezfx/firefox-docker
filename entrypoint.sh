a5231ba66071225f816fed20e39f12da0c4802c9
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2022-12-01 10:38:54 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ USER=firefox
 USERID=1000
 echo "creating new $USER with UID $USERID"
 useradd -m $USER -u $USERID 
-chown -R $USER /home/$USER
-chown -R $USER /Applications/Firefox_Web_Browser-x86_64.AppImage
-chgrp -R $USER /Applications/Firefox_Web_Browser-x86_64.AppImage
+chown -Rf $USER /home/$USER
+chown -Rf $USER /Applications/
+chgrp -Rf $USER /Applications/
 cd /home/$USER 
-su $USER -c "/Applications/Firefox_Web_Browser-x86_64.AppImage --appimage-extract-and-run"
+su $USER -c "/Applications/firefox/firefox"
