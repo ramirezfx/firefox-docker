4c21eddf46adc0eae1a37e2fa720b79d3cb91def
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2022-12-01 09:30:25 +0100

    Update startfirefox.sh

--- startfirefox.sh
+++ startfirefox.sh
@@ -5,20 +5,21 @@ HOME="$(echo ~)"
 set -e
 
 if [[ -n "$(docker ps -qaf 'name=ramirezfx/firefox:latest')" ]]; then
-	docker restart ramirezfx/firefox:latest
+        docker restart ramirezfx/firefox:latest
 else
-	USER_UID=$(id -u)
-	USER_GID=$(id -g)
-	xhost +local:docker
+        USER_UID=$(id -u)
+        USER_GID=$(id -g)
+        xhost +local:docker
 
-	docker run --shm-size=2g --rm \
+        docker run --shm-size=2g --rm \
                 --security-opt seccomp=unconfined \
-		--env=USER_UID=$USER_UID \
-		--env=USER_GID=$USER_GID \
-		--env=DISPLAY=unix$DISPLAY \
-		-v ${HOME}/docker/firefox-home:/home/firefox \
-		--volume=/tmp/.X11-unix:/tmp/.X11-unix:ro \
-		--volume=/run/user/$USER_UID/pulse:/run/pulse:ro \
-		--name firefox \
-		ramirezfx/firefox:latest
+                --env=USER_UID=$USER_UID \
+                --env=USER_GID=$USER_GID \
+                --env=DISPLAY=unix$DISPLAY \
+                -v firefox-appdir:/Applications \
+                -v ${HOME}/docker/firefox-home:/home/firefox \
+                --volume=/tmp/.X11-unix:/tmp/.X11-unix:ro \
+                --volume=/run/user/$USER_UID/pulse:/run/pulse:ro \
+                --name firefox \
+                ramirezfx/firefox:latest
 fi
